2d8d17df3a298cadd606a149d6c91a656583e5e7
Vasiliy <cyanide70@yandex.ru>
2018-01-23 23:13:25 +0300

    Добавлены функции через интерфейсы

--- Calculator/ConsoleCalculator/Program.cs
+++ Calculator/ConsoleCalculator/Program.cs
@@ -1,195 +1,56 @@
-﻿using System;
+﻿using ConsoleCalculator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleCalculator
+    
+namespace CalculatorLibrary
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> log = new List<string>();
+            var oper = "";
+            double x;
+            double y;
 
-            CalculateComand comand = new CalculateComand();
-            var calculate = new Calculate();
-
-            Console.WriteLine("Введите команду вида Оператор Число1 Число2,\n " +
-                                    "для получения доступных операторов введите: !help,\n " +
-                                    "для вывода последних операций: !last");
-
-            
-            while(true)
+            var operations = new List<string>()
             {
-                string stroke = "";
-                stroke = Console.ReadLine();
-                
-                switch (stroke)
-                {
-                    case "!help":
-                        {
-                            string[] commands = new string[]
-                           {
-                                "Список команд",
-                                "1. !help список команд",
-                                "2. !log последние операции",
-                                "3. !sum сложение",
-                                "4. !sub вычитание",
-                                "5. !mul умножение",
-                                "6. !div деление",
-                           };
-                            foreach (var str in commands)
-                                Console.WriteLine(str);
+                "sum - сумма", "sub - разность", "div - деление", "sqrt - корень степени n"
+            };
 
-                            ////log.Add("!help");
-                        }                       
-                        break;
-
-                    case "!log":
-                        {
-                            foreach (var str in log)
-                                Console.WriteLine(str);
-                            ////log.Add("!log");
-                        }
-                        break;
-
-                    case "!sum":
-                        {
-                            Console.WriteLine("Введите первое число:");
-                            if (Double.TryParse(Console.ReadLine(), out double x))
-                            {
-                                Console.WriteLine("Введите второе число:");
-
-                                if (Double.TryParse(Console.ReadLine(), out double y))
-                                {
-                                    var result = calculate.Sum(x, y);
-                                    Console.WriteLine("{0} + {1} = {2}", x, y, result);                          
-                                    log.Add(x + " + " + y + " = " + result);
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Введите число!");
-                                    log.Add("Введите число!");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Введите число!");
-                                log.Add("Введите число!");
-                            }
-                        }
-                        break;
-
-                    case "!sub":
-                        {
-                            Console.WriteLine("Введите первое число:");
-                            if (Double.TryParse(Console.ReadLine(), out double x))
-                            {
-                                Console.WriteLine("Введите второе число:");
-
-                                if (Double.TryParse(Console.ReadLine(), out double y))
-                                {
-                                    var result = calculate.Sub(x, y);
-                                    Console.WriteLine("{0} - {1} = {2}", x, y, result);
-                                    log.Add(x + " - " + y + " = " + result);
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Введите число!");
-                                    log.Add("Введите число!");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Введите число!");
-                                log.Add("Введите число!");
-                            }
-                            break;
-                        }
-
-                    case "!mul":
-                        {
-                            Console.WriteLine("Введите первое число:");
-                            if(Double.TryParse(Console.ReadLine(), out double x))
-                            {
-                                Console.WriteLine("Введите второе число:");
-
-                                if (Double.TryParse(Console.ReadLine(), out double y))
-                                {
-                                    var result = calculate.Mul(x, y);
-                                    Console.WriteLine("{0} * {1} = {2}", x, y, result);
-                                    log.Add(x + " * " + y + " = " + result);
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Введите число!");
-                                    log.Add("Введите число!");
-                                }
-                            }  
-                            else
-                            {
-                                Console.WriteLine("Введите число!");
-                                log.Add("Введите число!");
-                            }
-                        }
-                        break;
-
-                    case "!div":
-                        {
-                            Console.WriteLine("Введите первое число:");
-                            if (Double.TryParse(Console.ReadLine(), out double x))
-                            {
-                                Console.WriteLine("Введите второе число:");
-
-                                if (Double.TryParse(Console.ReadLine(), out double y))
-                                {
-                                    if (y != 0)
-                                    {
-                                        var result = calculate.Div(x, y);
-                                        Console.WriteLine("{0} / {1} = {2}", x, y, result);
-                                        log.Add(x + " / " + y + " = " + result);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Знаменатель не должен быть равен 0!");
-                                        log.Add("Знаменатель не должен быть равен 0!");
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Введите число!");
-                                    log.Add("Введите число!");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Введите число!");
-                                log.Add("Введите число!");
-                            }
-                        }
-                        break;
-
-                    default:
-                        {
-                            Console.WriteLine("Введите команду корректно!");
-                        }
-                        break;
-
-                }
+            if (args.Length > 0)
+            {
+                oper = args[0];
+                x = Convert.ToDouble(args[1]);
+                y = Convert.ToDouble(args[2]);
             }
+            else
+            {
+                Console.WriteLine("Выберите операцию:");
+                foreach (var item in operations)
+                {
+                    Console.WriteLine(item);
+                }
+
+                oper = Console.ReadLine();
+
+                Console.WriteLine("Введите данные в одну строку, разделяя числа пробелом");
+                var data = Console.ReadLine().Split(' ').Select(d => Convert.ToDouble(d)).ToArray();
+                x = data[0];
+                y = data.Length > 1 ? data[1] : 0;
+            }
+
+            Double result = double.NaN;
+
+            Calculate calculate = new Calculate();
+
+            result = calculate.Exec(oper, new[] { x.ToString(), y.ToString() });
             
 
+            Console.WriteLine($"{oper}({x}, {y}) = {result}");
 
-            //var c = args[0];
-            //var a = Convert.ToInt32(args[1]);
-            //var b = Convert.ToInt32(args[2]);
-            //if (c == "sum")
-            //{
-            //    var result = a + b;
-            //    Console.WriteLine(result);
-            //}
-            //Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
